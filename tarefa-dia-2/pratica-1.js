d773d6177c1a6c3c1b37bc1e953deee1248cff68
rafael freitas <rafashleymel778@gmail.com>
2022-08-25 19:58:29 -0300

    estou-comecando-a-entender

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
-let nome = rafael
-let email = rafashleymel778@gmail.com
+let nome = Rafael
+let email = rafashleymel778email.com
 let profissao = programador 
 let idade = 13
 let lugar = canada
+string = nome
+string = email
+string = profissao
+number = idade 
+string = lugar
