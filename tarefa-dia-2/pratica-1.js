daa29fa7be8ced96feebf5a0e393b34419ba7576
rafael freitas <rafashleymel778@gmail.com>
2022-08-25 22:50:56 -0300

    entao-isso-era-simples?

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,10 +1,15 @@
-let nome = Rafael
-let email = rafashleymel778email.com
-let profissao = programador 
-let idade = 13
-let lugar = canada
+let nome = "Rafael"
+let email = "rafashleymel778email.com"
+let profissao = "programador"
+let idade = "13"
+let lugar = "canada"
 string = nome
 string = email
 string = profissao
 number = idade 
 string = lugar
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
